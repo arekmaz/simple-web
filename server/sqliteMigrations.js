e75b6bbd2e40678ac2ecc3358365d845586ab2c2
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-10-15 17:13:23 +0200

    add fly config

--- server/sqliteMigrations.js
+++ server/sqliteMigrations.js
@@ -1,31 +1,31 @@
-import { db } from './db.js'
+import { db } from "./db.js";
 
 export const migrateDb = db.transaction((version) => {
+  const { user_version: dbVersion } = db.prepare("PRAGMA user_version").get();
 
-
-  const { user_version: dbVersion } = db.prepare('PRAGMA user_version').get();
-
-  console.log('SQLite Version:', dbVersion);
+  console.log("SQLite Version:", dbVersion);
 
   if (dbVersion >= version) {
     console.log(`SQLite Version ${version} up to date (db@${dbVersion})`);
-    return
-  } 
+    return;
+  }
 
   console.log(`SQLite Migrating ${dbVersion}->${version}`, version);
 
   if (version >= 1) {
-    db.prepare(`
+    db.prepare(
+      `
       CREATE TABLE IF NOT EXISTS todos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         done INTEGER NOT NULL DEFAULT 0,  -- 0 for false, 1 for true
         description TEXT
       )
-    `).run()
+    `,
+    ).run();
   }
 
-  db.prepare(`PRAGMA user_version = ${version}`).run()
+  db.prepare(`PRAGMA user_version = ${version}`).run();
 
   console.log(`SQLite Migration Done`);
-})
+});
